2489711b49f62d039acd6a95184d537c0edebc2b
Sangavi-22 <104341990+Sangavi-22@users.noreply.github.com>
2022-07-07 13:47:17 +0530

    Delete Controller directory

--- Controller/ControlLogin/LoginControllerService.java
+++
@@ -1,9 +0,0 @@
-package Controller.ControlLogin;
-
-public interface LoginControllerService {
-    void goToHomePage();
-    void setAccountName(String accountName);
-    String getAccountName();
-    void removeUser(String userName);
-    void logOut(String userName);
-}
